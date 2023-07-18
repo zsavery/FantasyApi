86cc1fa8b259957840647cb69be4746fb80fd3b2
zsavery <51015959+zsavery@users.noreply.github.com>
2023-07-18 14:42:28 -0500

    Add files via upload

---
+++ Dungeon-Epic/src/main/java/com/zsavery/Dungeon/Epic/DungeonEpicApplication.java
@@ -0,0 +1,13 @@
+package com.zsavery.Dungeon.Epic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonEpicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonEpicApplication.class, args);
+	}
+
+}
---
+++ Dungeon-Epic/src/test/java/com/zsavery/Dungeon/Epic/DungeonEpicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsavery.Dungeon.Epic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonEpicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
